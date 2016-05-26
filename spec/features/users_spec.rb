8388e8c3cb04db3af0ecd01889a645239a3516b5
Екатерина Иониди <ekaterinaionidi@MacBook-Pro-Ekaterina.local>
2016-05-26 23:17:45 +0300

    added features tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -53,4 +53,54 @@ feature 'users', js: true do
       expect(page.body).not_to match "<tr><td>Name:</td><td>#{user.name}</td></tr>"
     end
   end
+  
+  #сценарий просмотра пользователя
+  context 'view user' do
+    scenario 'correct case' do
+      user = create :user
+      visit '/users'
+      expect(page).to have_link user.id
+      click_on user.id
+      expect(page.find('header').text).to eq "User ##{user.id}"
+      expect(page.body).to match user.email
+      expect(page.body).to match user.name
+    end
+  end
+  
+  #сценарий редактирования пользователя
+  context 'edit user' do
+    scenario 'correct case' do
+      user = create :user
+      visit '/users'
+      expect(page).to have_link user.id
+      click_on user.id
+      expect(page).to have_link 'edit'
+      #expect(page).to have_selector 'input[value="edit"]'
+      click_on 'edit'
+      #expect(page.body).to match "Edit User ##{user.id}"
+      expect(page).to have_selector 'input[value="update"]'
+      page.fill_in 'user[email]', with: 'newemail@name.new'
+      find('input[value="update"]').click
+      expect(page.body).to match "User ##{user.id}"
+      expect(page.body).to match 'newemail@name.new'
+      expect(page.body).to match user.name
+    end
+  
+    scenario 'incorrect case' do
+      user = create :user
+      visit '/users'
+      expect(page).to have_link user.id
+      click_on user.id
+      expect(page).to have_link 'edit'
+      click_on 'edit'
+      #expect(page.find('header').text).to eq "Edit User ##{user.id}"
+      expect(page).to have_selector 'input[value="update"]'
+      page.fill_in 'user[email]', with: 'wrong_email'
+      find('input[value="update"]').click
+      expect(page.find('header').text).to eq "Edit User ##{user.id}"
+      expect(page.body).to match 'wrong_email'
+      expect(page.body).to match user.name
+      expect(page.find('.flash-error').text).not_to be_empty
+    end
+  end
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+describe Post do
+  context 'validation' do
+    def post_not_valid(post, wrong_attr)
+      expect(post.valid?).to be false
+      expect(post.save).to be false
+      expect(post.errors[wrong_attr]).not_to be_empty
+    end
+
+    def post_valid(post)
+      expect(post.valid?).to be true
+      expect(post.save).to be true
+      expect(post.errors).to be_empty
+    end
+
+    describe 'on post' do
+      it 'should not save post w/o author' do
+	    post_not_valid Post.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph), :author
+      end
+
+      it 'should not save post w/o content' do
+        post_not_valid FactoryGirl.build(:post, :with_user, content: nil), :content
+      end
+
+      it 'should not save short post' do
+        post_not_valid Post.new(title: Faker::Lorem.sentence, content: "a" * 5, author: create(:user)), :content
+      end
+
+      it 'should not save long post' do
+        post_not_valid Post.new(title: Faker::Lorem.sentence, content: "a" * 2050, author: create(:user)), :content
+      end
+
+      it 'should save post w/minimal content' do
+        post_valid Post.new(title: Faker::Lorem.sentence, content: "a" * 8, author: create(:user))
+      end
+
+      it 'should save post w/maximum content' do
+        post_valid Post.new(title: Faker::Lorem.sentence, content: "a" * 2048, author: create(:user))
+      end
+      
+      it 'should not save post w/o title' do
+	    post_not_valid Post.new(content: Faker::Lorem.paragraph, author: create(:user)), :title
+      end
+
+      it 'should not save post w/short title' do
+        post_not_valid Post.new(content: Faker::Lorem.paragraph, title: "aa", author: create(:user)), :title
+      end
+
+      it 'should not save post w/long title' do
+        post_not_valid Post.new(content: Faker::Lorem.paragraph, title: "a" * 260, author: create(:user)), :title
+      end
+
+      it 'should save post w/minimal title' do
+        post_valid Post.new(content: Faker::Lorem.paragraph, title: "aaa", author: create(:user))
+      end
+
+      it 'should save post w/maximum title' do
+        post_valid Post.new(title: Faker::Lorem.sentence, content: "a" * 255, author: create(:user))
+      end
+    end
+  end
+  
+  context 'association' do
+    describe 'posts' do
+      it 'should ' do
+        
+      end
+    end
+  end
+  
+  context 'method' do
+    describe 'author of any role' do
+      it 'should create post' do
+        user = create :user, :user
+        moderator = create :user, :moderator
+        admin = create :user, :admin
+        user_post = create :post, author: user
+        moderator_post = FactoryGirl.create :post, author: moderator
+        admin_post = create :post, author: admin
+        expect(user_post.author_role).to eq ''
+        expect(moderator_post.author_role).to eq 'moderator'
+        expect(admin_post.author_role).to eq 'admin'
+      end
+    end
+    
+    describe 'posts' do
+      it 'should return author name' do
+        user = create :user, :user
+        user_post = create :post, author: user
+        expect(user_post.author_name).to eq user.name
+      end
+    end
+  end
+end 
