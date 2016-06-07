74a14a84a4a1f43fdaf0224168ba464caac45d58
Mikhail Alexeev <mikhail.alexeev.dmr@gmail.com>
2016-06-07 12:20:56 +0300

    Fix registration & login. Modify User.edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from ActiveRecord::RecordNotFound do |_|
     respond_to do |f|
       f.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
       f.json { render nothing: true, status: 404 }
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_post, only: %i(show edit update destroy)
 
   def index
