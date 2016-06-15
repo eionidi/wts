64f1e43206c532045e1fe3f62a854ff8b59a9442
Mikhail Alexeev <malexeev@artec-group.com>
2016-06-15 11:25:33 +0300

    Change config for paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 
   has_attached_file :image,
                     styles: { index: '200x200>', show: '400x400>' },
-                    url: "/paperclip/post_image/:id/:style.:extension"
+                    url: "/paperclip/#{Rails.env}/post_image/:id/:style.:extension"
   validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\Z}
 
   validates :title, presence: true, length: { in: 3..255 }
