d5724ee2de02c05d64d6565667ac70a8fb600ac8
Mikhail Alexeev <mikhail.alexeev.dmr@gmail.com>
2016-06-24 08:05:23 +0300

    Comments II

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,28 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
-  load_and_authorize_resource
+  load_and_authorize_resource :post
+  load_and_authorize_resource through: :post
+
+  skip_load_resource only: %i(create)
 
   def new
   end
 
   def create
+    @comment = @post.comments.new params.require(:comment).permit(:content, :file_attach)
+    @comment.assign_attributes author: current_user
+    if @comment.save
+      flash.now[:notice] = "Comment ##{@comment.id} created!"
+      redirect_to post_path(@post)
+    else
+      flash.now[:error] = "Errors: #{@comment.errors.full_messages.join ', '}"
+      render :new
+    end
   end
 
   def show
+    @post = @post.includes :comments
   end
 
   def edit
