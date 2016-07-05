f2dedec4659034a1aaee6fdb177f485cf0e036ea
Екатерина Иониди <ekaterinaionidi@MacBook-Pro-Ekaterina.local>
2016-07-05 18:40:14 +0300

    added shared exs and corrections

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -27,7 +27,7 @@ describe CommentsController do
     {
       content: Faker::Lorem.paragraph,
       author: create(:user),
-      post: create(:post)
+      post: create(:post, :with_user)
     }
   end
   let(:comment_params) do
@@ -57,145 +57,152 @@ describe CommentsController do
       get :show, id: (Comment.last.try(:id) || 0) + 1
       expect(response).to have_http_status(404)
     end
+
+    it 'should redirect to My Artec' do
+    # ???
+      stub_request(:get, "http://www.staging-booth-my.artec3d.com/users:80/").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => "", :headers => {})
+    end
   end
 
-  # shared_examples 'create comment' do |role|
-  #   it "with role '#{role}'" do
-  #     sign_in users[role.to_sym]
-  #     expect { comment :create, :with_user, comment: comment_attrs }.to change { Comment.count }.by 1
-  #     comment = Comment.last
-  #     expect(response).to redirect_to "posts/#{post.id}"
-  #     expect(flash.now[:notice]).to eq "Comment ##{@comment.id} created!"
-  #     expect(comment.author).to eq users[role.to_sym]
-  #   end
-  # end
+  shared_examples 'create comment' do |role|
+    it "with role '#{role}'" do
+      sign_in users[role.to_sym]
+      expect { post :create, comment: comment_attrs }.to change { Comment.count }.by 1
+      comment = Comment.last
+      expect(response).to redirect_to "posts/#{post.id}"
+      expect(flash.now[:notice]).to eq "Comment ##{@comment.id} created!"
+      expect(comment.author).to eq users[role.to_sym]
+    end
+  end
 
-  # describe '#create' do
-  #   User.roles.keys.each { |role| it_behaves_like 'create comment', role }
+  describe '#create' do
+    User.roles.keys.each { |role| it_behaves_like 'create comment', role }
 
-  #   it 'should save with attach' do
-  #     sign_in users.values.sample
-  #     expect { comment :create, :with_user,
-  #              comment: comment_attrs.merge(image: fixture_file_upload('fixtures/post_image.png', 'image/png')) }.
-  #       to change { Comment.count }.by 1
-  #     expect(Comment.last.image).to be_exists
-  #   end
-  # end
+    it 'should save with attach' do
+      sign_in users.values.sample
+      expect { post :create,
+               comment: comment_attrs.merge(image: fixture_file_upload('fixtures/post_image.png', 'image/png')) }.
+        to change { Comment.count }.by 1
+      expect(Comment.last.image).to be_exists
+    end
+  end
 
-  # describe '#destroy' do
-  #   it 'admin should destroy comment' do
-  #     sign_in users[:admin]
-  #     comment = create :comment, :with_user
-  #     expect { delete :destroy, id: comment.id }.to change { Comment.count }.by -1
-  #     expect(response).to redirect_to '/posts'
-  #   end
+  describe '#destroy' do
+    it 'admin should destroy comment' do
+      sign_in users[:admin]
+      comment = create :comment, :with_user
+      expect { delete :destroy, id: comment.id }.to change { Comment.count }.by -1
+      expect(response).to redirect_to '/posts'
+    end
 
-  #   it 'admin should not destroy comment w/wrong id' do
-  #     sign_in users[:admin]
-  #     comments.values.each(&:reload)
-  #     expect { delete :destroy, id: (Comment.last.try(:id) || 0) + 1 }.to not_change { Comment.count }
-  #     expect(response).to have_http_status(404)
-  #   end
+    it 'admin should not destroy comment w/wrong id' do
+      sign_in users[:admin]
+      comments.values.each(&:reload)
+      expect { delete :destroy, id: (Comment.last.try(:id) || 0) + 1 }.to not_change { Comment.count }
+      expect(response).to have_http_status(404)
+    end
 
-  #   it 'user should not destroy comment' do
-  #     sign_in users[:user]
-  #     comment = create :comment, :with_user
-  #     expect { delete :destroy, id: comment.id }.to not_change { Comment.count }
-  #     expect(response).to redirect_to '/'
-  #   end
+    it 'user should not destroy comment' do
+      sign_in users[:user]
+      comment = create :comment, :with_user
+      expect { delete :destroy, id: comment.id }.to not_change { Comment.count }
+      expect(response).to redirect_to '/'
+    end
 
-  #   it 'moderator should not destroy post' do
-  #     sign_in users[:moderator]
-  #     comment = create :comment, :with_user
-  #     expect { delete :destroy, id: comment.id }.to not_change { Comment.count }
-  #     expect(response).to redirect_to '/'
-  #   end
-  # end
+    it 'moderator should not destroy post' do
+      sign_in users[:moderator]
+      comment = create :comment, :with_user
+      expect { delete :destroy, id: comment.id }.to not_change { Comment.count }
+      expect(response).to redirect_to '/'
+    end
+  end
 
-  # shared_examples 'update comment' do |attr_name|
-  #   it "with empty '#{attr_name}'" do
-  #     sign_in users[:admin]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: comment.id, comment: comment_params.merge(attr_name => '')
-  #     comment.reload
-  #     expect(comment.content).to eq comment_attrs[:content]
-  #     expect(response).to render_template 'edit'
-  #     expect(flash[:error]).not_to be_empty
-  #   end
-  # end
+  shared_examples 'update comment' do |attr_name|
+    it "with empty '#{attr_name}'" do
+      sign_in users[:admin]
+      comment = create :comment, comment_attrs
+      patch :update, id: comment.id, comment: comment_params.merge(attr_name => '')
+      comment.reload
+      expect(comment.content).to eq comment_attrs[:content]
+      expect(response).to render_template 'edit'
+      expect(flash[:error]).not_to be_empty
+    end
+  end
 
-  # shared_examples'update own comment' do |role|
-  #   it "with role '#{role}'" do
-  #     sign_in users[role.to_sym]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: comments[role.to_sym].id, comment: comment_params
-  #     post.reload
-  #     expect(comment.content).to eq comment_params[:content]
-  #     expect(response).to redirect_to "/posts/#{post.id}"
-  #     expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
-  #   end
-  # end
+  shared_examples'update own comment' do |role|
+    it "with role '#{role}'" do
+      sign_in users[role.to_sym]
+      comment = create :comment, comment_attrs
+      patch :update, id: comments[role.to_sym].id, comment: comment_params
+      post.reload
+      expect(comment.content).to eq comment_params[:content]
+      expect(response).to redirect_to "/posts/#{post.id}"
+      expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
+    end
+  end
 
-  # describe '#update' do
-  #   User.roles.keys.each { |role| it_behaves_like 'update own comment', role }
+  describe '#update' do
+    User.roles.keys.each { |role| it_behaves_like 'update own comment', role }
 
-  #   it 'user should not update someones comment' do
-  #     sign_in users[:user]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: comment.id, comment: comment_params
-  #     comment.reload
-  #     expect(comment.content).to eq comment_attrs[:content]
-  #   end
+    it 'user should not update someones comment' do
+      sign_in users[:user]
+      comment = create :comment, comment_attrs
+      patch :update, id: comment.id, comment: comment_params
+      comment.reload
+      expect(comment.content).to eq comment_attrs[:content]
+    end
 
-  #   it 'admin should update someones comment' do
-  #     sign_in users[:admin]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: comment.id, comment: comment_params
-  #     post.reload
-  #     expect(comment.content).to eq comment_params[:content]
-  #     expect(response).to redirect_to "/posts/#{post.id}"
-  #     expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
-  #   end
+    it 'admin should update someones comment' do
+      sign_in users[:admin]
+      comment = create :comment, comment_attrs
+      patch :update, id: comment.id, comment: comment_params
+      post.reload
+      expect(comment.content).to eq comment_params[:content]
+      expect(response).to redirect_to "/posts/#{post.id}"
+      expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
+    end
 
-  #   it 'moderator should update someones comment' do
-  #     sign_in users[:moderator]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: comment.id, comment: comment_params
-  #     post.reload
-  #     expect(comment.content).to eq comment_params[:content]
-  #     expect(response).to redirect_to "/posts/#{post.id}"
-  #     expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
-  #   end
+    it 'moderator should update someones comment' do
+      sign_in users[:moderator]
+      comment = create :comment, comment_attrs
+      patch :update, id: comment.id, comment: comment_params
+      post.reload
+      expect(comment.content).to eq comment_params[:content]
+      expect(response).to redirect_to "/posts/#{post.id}"
+      expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
+    end
 
-  #   it 'should save updated_at' do
-  #     sign_in users[:admin]
-  #     comment = create :comment, comment_attrs
-  #     time = Faker::Time.between 1.year.ago, 1.year.from_now
-  #     Timecop.freeze time
-  #     expect { patch :update, id: comment.id, comment: comment_params }.to change { comment.reload.updated_at.to_i }.to time.to_i
-  #     Timecop.return
-  #   end
+    it 'should save updated_at' do
+      sign_in users[:admin]
+      comment = create :comment, comment_attrs
+      time = Faker::Time.between 1.year.ago, 1.year.from_now
+      Timecop.freeze time
+      expect { patch :update, id: comment.id, comment: comment_params }.to change { comment.reload.updated_at.to_i }.to time.to_i
+      Timecop.return
+    end
 
-  #   it 'should ignore not permitted attrs' do
-  #     sign_in users[:admin]
-  #     comment = create :comment, comment_attrs
-  #     old_id = comment.id.freeze
-  #     patch :update, id: comment.id, comment: comment_params.merge(id: (Comment.last.try(:id) || 0) + 1)
-  #     post.reload
-  #     expect(post.content).to eq post_params[:content]
-  #     expect(response).to redirect_to "/posts/#{post.id}"
-  #     expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
-  #     expect(comment.id).to eq old_id
-  #   end
+    it 'should ignore not permitted attrs' do
+      sign_in users[:admin]
+      comment = create :comment, comment_attrs
+      old_id = comment.id.freeze
+      patch :update, id: comment.id, comment: comment_params.merge(id: (Comment.last.try(:id) || 0) + 1)
+      post.reload
+      expect(post.content).to eq post_params[:content]
+      expect(response).to redirect_to "/posts/#{post.id}"
+      expect(flash[:notice]).to eq "Comment ##{@comment.id} updated!"
+      expect(comment.id).to eq old_id
+    end
 
-  #   it 'should not update w/wrong comment id' do
-  #     sign_in users[:admin]
-  #     comment = create :comment, comment_attrs
-  #     patch :update, id: ((Comment.last.try(:id) || 0) + 1), comment: comment_params
-  #     post.reload
-  #     expect(comment.content).to eq comment_attrs[:content]
-  #     expect(response).to have_http_status(404)
-  #   end
-  #   %i(title content).each { |attr_name| it_behaves_like 'update comment', attr_name }
-  #end
+    it 'should not update w/wrong comment id' do
+      sign_in users[:admin]
+      comment = create :comment, comment_attrs
+      patch :update, id: ((Comment.last.try(:id) || 0) + 1), comment: comment_params
+      post.reload
+      expect(comment.content).to eq comment_attrs[:content]
+      expect(response).to have_http_status(404)
+    end
+    %i(title content).each { |attr_name| it_behaves_like 'update comment', attr_name }
+  end
 end
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -3,51 +3,78 @@ require 'rails_helper'
 feature 'comments', js: true do
   let(:user) { create :user, :admin }
   let(:post) { create :post, :with_user }
+  let(:users) do
+    {
+      user: create(:user, :user),
+      moderator: create(:user, :moderator),
+      admin: create(:user, :admin)
+    }
+  end
 
 
   before(:each) { login_as user }
 
+  shared_examples 'create comment' do |role|
+    scenario "correct case with role '#{role}'" do
+      login_as users[role.to_sym]
+      post = create :post, :with_user
+      visit "/posts/#{post.id}"
+      expect(page).to have_link 'add comment'
+      click_on 'add comment'
+      expect(page).to have_field 'comment[content]'
+      expect(page).to have_selector 'input[value="create"]'
+      page.fill_in 'comment[content]', with: 'My first comment'
+      find('input[value="create"]').click
+      expect(page.find('header').text).to match "Post ##{post.id}"
+      expect(page.body).to match 'My first comment'
+    end
+  end
+
   context 'create comment' do
-    scenario 'correct case for admin' do
-      login_as create(:user, :admin)
-      post = create :post, :with_user
-      visit "/posts/#{post.id}"
-      expect(page).to have_link 'add comment'
-      click_on 'add comment'
-      expect(page).to have_field 'comment[content]'
-      expect(page).to have_selector 'input[value="create"]'
-      page.fill_in 'comment[content]', with: 'My first comment'
-      find('input[value="create"]').click
-      expect(page.find('header').text).to match "Post ##{post.id}"
-      expect(page.body).to match 'My first comment'
-    end
+    User.roles.keys.each { |role| it_behaves_like 'create comment', role }
 
-    scenario 'correct case for user' do
-      post = create :post, :with_user
-      visit "/posts/#{post.id}"
-      expect(page).to have_link 'add comment'
-      click_on 'add comment'
-      expect(page).to have_field 'comment[content]'
-      expect(page).to have_selector 'input[value="create"]'
-      page.fill_in 'comment[content]', with: 'My first comment'
-      find('input[value="create"]').click
-      expect(page.find('header').text).to match "Post ##{post.id}"
-      expect(page.body).to match 'My first comment'
-    end
 
-    scenario 'correct case for moderator' do
-      login_as create(:user, :moderator)
-      post = create :post, :with_user
-      visit "/posts/#{post.id}"
-      expect(page).to have_link 'add comment'
-      click_on 'add comment'
-      expect(page).to have_field 'comment[content]'
-      expect(page).to have_selector 'input[value="create"]'
-      page.fill_in 'comment[content]', with: 'My first comment'
-      find('input[value="create"]').click
-      expect(page.find('header').text).to match "Post ##{post.id}"
-      expect(page.body).to match 'My first comment'
-    end
+  # context 'create comment' do
+  #   scenario 'correct case for admin' do
+  #     login_as create(:user, :admin)
+  #     post = create :post, :with_user
+  #     visit "/posts/#{post.id}"
+  #     expect(page).to have_link 'add comment'
+  #     click_on 'add comment'
+  #     expect(page).to have_field 'comment[content]'
+  #     expect(page).to have_selector 'input[value="create"]'
+  #     page.fill_in 'comment[content]', with: 'My first comment'
+  #     find('input[value="create"]').click
+  #     expect(page.find('header').text).to match "Post ##{post.id}"
+  #     expect(page.body).to match 'My first comment'
+  #   end
+
+  #   scenario 'correct case for user' do
+  #     post = create :post, :with_user
+  #     visit "/posts/#{post.id}"
+  #     expect(page).to have_link 'add comment'
+  #     click_on 'add comment'
+  #     expect(page).to have_field 'comment[content]'
+  #     expect(page).to have_selector 'input[value="create"]'
+  #     page.fill_in 'comment[content]', with: 'My first comment'
+  #     find('input[value="create"]').click
+  #     expect(page.find('header').text).to match "Post ##{post.id}"
+  #     expect(page.body).to match 'My first comment'
+  #   end
+
+  #   scenario 'correct case for moderator' do
+  #     login_as create(:user, :moderator)
+  #     post = create :post, :with_user
+  #     visit "/posts/#{post.id}"
+  #     expect(page).to have_link 'add comment'
+  #     click_on 'add comment'
+  #     expect(page).to have_field 'comment[content]'
+  #     expect(page).to have_selector 'input[value="create"]'
+  #     page.fill_in 'comment[content]', with: 'My first comment'
+  #     find('input[value="create"]').click
+  #     expect(page.find('header').text).to match "Post ##{post.id}"
+  #     expect(page.body).to match 'My first comment'
+  #   end
 
     scenario 'incorrect case' do
       login_as create(:user, :admin)
@@ -68,11 +95,11 @@ feature 'comments', js: true do
     scenario 'correct case for admin' do
       login_as create(:user, :admin)
       #post = create :post, :with_user
-   	  comment = Comment.new(content: Faker::Lorem.paragraph, post: create(:post, :with_user))
+   	  comment = Comment.create(content: Faker::Lorem.paragraph, post: create(:post, :with_user))
+   	  comment.errors.messages
       #comment = create :comment, :with_user
       visit "/posts/#{post.id}"
       #not found
-      puts(page.body)
       expect(page).to have_link comment.content 
       click_on comment.content
       expect(page.find('header').text).to eq "Comment ##{comment.id}"
@@ -103,12 +130,29 @@ feature 'comments', js: true do
     end
   end
 
+  # shared_examples 'view comment' do
+  #   scenario "correct case with role '#{role}'"
+  # 	login_as users[role.to_sym]
+  # 	post = create :post, :with_user
+  #     comment = create :comment, :with_user
+  #     visit "/posts/#{post.id}/comments/#{comment.id}" 
+  #     expect(page.find('header').text).to eq "Comment ##{comment.id} on post ##{post.id}"
+  #     expect(page.body).to match comment.content
+  #   end
+  # end
+
+  # context 'view comment' do
+  #   User.roles.keys.each { |role| it_behaves_like 'view comment', role }
+  # end
+
   context 'view comment' do
     scenario 'correct case for user' do
       login_as create(:user, :user)
       post = create :post, :with_user
       comment = create :comment, :with_user
       visit "/posts/#{post.id}/comments/#{comment.id}"
+      puts(page.body)
+      #save_and_open_page
       expect(page.find('header').text).to eq "Comment ##{comment.id} on post ##{post.id}"
       expect(page.body).to match comment.content
     end
@@ -116,7 +160,7 @@ feature 'comments', js: true do
     scenario 'correct case for admin' do
       login_as create(:user, :admin)
       comment = create :comment, :with_user
-      visit "/posts/#{post.id}"
+      visit "/posts/#{post.id}/comments/#{comment.id}"
       expect(page.find('header').text).to eq "Comment ##{comment.id} on post ##{post.id}"
       expect(page.body).to match comment.content
     end
@@ -124,11 +168,32 @@ feature 'comments', js: true do
     scenario 'correct case for moderator' do
       login_as create(:user, :moderator)
       comment = create :comment, :with_user
-      visit "/posts/#{post.id}"
+      visit "/posts/#{post.id}/comments/#{comment.id}"
       expect(page.find('header').text).to eq "Comment ##{comment.id} on post ##{post.id}"
       expect(page.body).to match comment.content
     end
   end
+
+  shared_examples 'edit comment' do
+  	scenario "correct case with role '#{role}'"
+      login_as users[role.to_sym]
+      comment = create :comment, :with_user
+      visit "posts/#{post.id}/comments/#{comment.id}"
+      expect(page).to have_link 'edit'
+      click_on 'edit'
+      expect(page.body).to match "Edit comment ##{comment.id}"
+      expect(page).to have_selector 'input[value="update"]'
+      page.fill_in 'comment[content]', with: 'New content'
+      find('input[value="update"]').click
+      expect(page.body).to match "Comment ##{comment.id}"
+      expect(page.body).to match 'Updated comment'
+    end
+  end
+
+  context 'edit comment' do
+  	User.roles.keys.each { |role| it_behaves_like 'edit comment', role }
+  end
+
 
   context 'edit comment' do
     scenario 'correct case for admin' do
@@ -151,7 +216,6 @@ feature 'comments', js: true do
       comment = create :comment, :with_user
       #404
       visit "posts/#{post.id}/comments/#{comment.id}"
-      puts(page.body)
       expect(page).to have_link 'edit'
       click_on 'edit'
       expect(page.body).to match "Edit comment ##{comment.id}"
@@ -167,7 +231,7 @@ feature 'comments', js: true do
       comment = create :comment, :with_user
       #404
       visit "posts/#{post.id}/comments/#{comment.id}"
-      puts(page.body)
+      puts("posts/#{post.id}/comments/#{comment.id}")
       expect(page).to have_link 'edit'
       click_on 'edit'
       expect(page.body).to match "Edit comment ##{comment.id}"
